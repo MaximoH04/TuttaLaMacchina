30ceffb7c0e279fcab458268386c549018029a6d
juanmaferreiro <111914980+juanmaferreiro@users.noreply.github.com>
2024-06-29 17:07:30 -0300

    Update Debito.java

--- MECANICA/Tutta la macchina/src/negocio/negocio/Debito.java
+++ MECANICA/Tutta la macchina/src/negocio/negocio/Debito.java
@@ -1,11 +1,29 @@
 package negocio;
-
 public class Debito extends MedioDePago {
+    private String numeroTarjeta;
+    private String vencimiento;
 
-    public Debito(double monto) {
+    public Debito(double monto, String numeroTarjeta, String vencimiento) {
         super(monto);
+        this.numeroTarjeta = numeroTarjeta;
+        this.vencimiento = vencimiento;
     }
 
+    public String getNumeroTarjeta() {
+        return numeroTarjeta;
+    }
+
+    public void setNumeroTarjeta(String numeroTarjeta) {
+        this.numeroTarjeta = numeroTarjeta;
+    }
+
+    public String getVencimiento() {
+        return vencimiento;
+    }
+
+    public void setVencimiento(String vencimiento) {
+        this.vencimiento = vencimiento;
+    }
     @Override
     public double calcularMontoFinal() {
         // No hay recargo ni descuento para débito
