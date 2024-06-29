a671ac1de9ca3a19462830e8d1ff0dbe376c9434
juanmaferreiro <111914980+juanmaferreiro@users.noreply.github.com>
2024-06-29 17:20:34 -0300

    Update Debito.java

--- MECANICA/Tutta la macchina/src/negocio/negocio/Debito.java
+++ MECANICA/Tutta la macchina/src/negocio/negocio/Debito.java
@@ -2,11 +2,20 @@ package negocio;
 public class Debito extends MedioDePago {
     private String numeroTarjeta;
     private String vencimiento;
+    private String cvvv;
 
-    public Debito(double monto, String numeroTarjeta, String vencimiento) {
+    public Debito(double monto, String numeroTarjeta, String vencimiento,String cvvv) {
         super(monto);
         this.numeroTarjeta = numeroTarjeta;
         this.vencimiento = vencimiento;
+        this.cvvv=cvvv;
+    }
+    public String getCvv() {
+        return cvvv;
+    }
+
+    public void setCvv(String cvv) {
+        this.cvvv = cvv;
     }
 
     public String getNumeroTarjeta() {
